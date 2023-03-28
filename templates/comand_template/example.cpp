51af3cc3510ab64d970408064e659ec5f88a1c82
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-03-28 19:11:20 +0100

    Add example of a command
    Add an example of a command to the template folder. This is to help
    people who are new to the project to understand how to add a new
    command.
    
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

---
+++ templates/comand_template/example.cpp
@@ -0,0 +1,21 @@
+/* Shell (example.cpp)
+ * A simple shell made in C++
+ * Github: https://www.github.com/awesomelewis2007/shell
+ *
+ * Shell example command source file
+*/
+
+
+#include <iostream>
+#include <string>
+
+#include "printsh.h"
+#include "config/config.h"
+
+
+/**
+ * example_command - Prints hello [arg] using the printsh function
+*/
+int example_command(std::string args){
+    printsh("hello "+args);
+}
