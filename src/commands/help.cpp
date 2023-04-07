e6fefe11aa7dc4c992f4483466e0b9378cdf940c
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-07 08:22:54 +0100

    Update history
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- src/commands/help.cpp
+++ src/commands/help.cpp
@@ -14,5 +14,6 @@
 void help(){
     std::cout << "awesomelewis2007's shell help" << std::endl;
     std::cout << "help - show this help message" << std::endl;
+    std::cout << "history - show the shell history" << std::endl;
     std::cout << "exit - exit the shell" << std::endl;
 }
