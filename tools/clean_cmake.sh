19ea0db97a76c30b5a8b1f7aebf1e0bbf74cf43d
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-01-12 15:48:12 +0000

    Add script
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

---
+++ tools/clean_cmake.sh
@@ -0,0 +1,12 @@
+# Shell (Makefile)
+# A simple shell made in C++
+# Github:https://www.github.com/awesomelewis2007/shell
+# Licence: GNU General Public License v3.0
+# By: Lewis Evans
+# Cmake build script
+
+mkdir build
+cd build
+cmake ..
+make clean
+cd ..
