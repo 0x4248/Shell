eb67f2d46713e44c4e12d37af18655804ed396bd
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-02-08 20:45:28 +0000

    Update comments
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -87,14 +87,14 @@ void print_welcome() {
  */
 int main(int argc, char *argv[]) {
     if (argc > 1) {
-        // Check for CLI arguments
+        /* Check for CLI arguments */
         if (strcmp(argv[1], "-v") == 0) {
-            // If the first CLI argument is "-v", print the version
+            /* If the first CLI argument is "-v", print the version */
             std::cout << parse_version() << std::endl;
             return 0;
         }
         if (strcmp(argv[1], "--config") == 0) {
-            // If the first CLI argument is "--config", print the config
+            /* If the first CLI argument is "--config", print the config */
             std::cout << "Shell configuration:" << std::endl;
             std::cout << "Name:\t\t\t" << CONFIG_NAME << std::endl;
             std::cout << "Version:\t\t" << parse_version() << std::endl;
@@ -110,18 +110,19 @@ int main(int argc, char *argv[]) {
             return 0;
         }
     }
-    // Print the welcome message
+    /* Print the welcome message */
     print_welcome();
     std::string output;
     std::string input;
     while (true) {
-        // Get user input
+        /* Command line prompt */
         std::cout << bold.green << get_username() << "@" << get_hostname()
                   << bold.reset << ":"
                   << format_current_directory(get_current_directory())
                   << bold.reset << "$ ";
+        /* Get the user's input */
         std::getline(std::cin, input);
-        // Run the user's input
+        /* Run the user's input */
         run_input(input);
     }
 }
